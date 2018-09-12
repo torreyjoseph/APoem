669d8629bc887d90099d55723b3c47f82701fe2d
torreyjoseph <42677917+torreyjoseph@users.noreply.github.com>
2018-09-11 22:23:01 -0700

    Poem Programme
    The programme containing the poem was added.

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("   l(a\n\n\n\n   le\n\n   af\n\n   fa\n\n\n\n   ll\n\n\n\n   s)\n\n   one\n\n   l\n\n\n\n   iness");
+    }
+}
