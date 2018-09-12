3d12879f8e27daeb310c42a2f33cb0d55c3963aa
torreyjoseph <42677917+torreyjoseph@users.noreply.github.com>
2018-09-12 09:16:42 -0700

    Final version of the poem code uploaded.
    The completed version of the poem printing code is here.

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("\tl(a\n\n\n\n\tle\n\n\taf\n\n\tfa\n\n\n\n\tll\n\n\n\n\ts)\n\n\tone\n\n\tl\n\n\n\n\tiness");
+    }
+}
